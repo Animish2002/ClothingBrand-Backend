f4a370a4844f597a47ccc991b00747b6fe980bb9
Animish2002 <animishchopade123@gmail.com>
2024-08-21 18:35:39 +0530

    new  changes

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -66,7 +66,6 @@ const login = async (req, res) => {
       maxAge: 3600000, // 1 hour expiration
     });
 
-    
     res.status(200).json({
       message: `Login success ${user.name} for role of ${user.role}`,
       success: true,
